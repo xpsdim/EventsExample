0f0bc7110f44ae267eca35319316602f1ef560bf
1 <1>
2017-07-20 13:41:50 +0300

    Add project files.

---
+++ EventsExample/MessageEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EventsExample
+{
+    //принято наследовать классы аргументов для обработчиков событий от EventArgs
+    //это наследование не несет никакой функциональной нагрузки,
+    //но так принято и это хорошая практика, см. также описание делегата Messanger.MesageSendHandler  
+    public class MessageEventArgs : EventArgs
+    {
+        public string SenderName { get; set; }
+
+        public string ReceiverName { get; set; }
+
+        public string Message { get; set; }        
+    }
+}
---
+++ EventsExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EventsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создаем инстанс нашего месенджера
+            var myMessanger = new Messanger();
+            //назначаем обработчик события OnMessageSend, в нашем случае это приватный статик метод нашей программы
+            //который просто будет логировать отправленные сообщения на екран
+            myMessanger.OnMessageSend += LogMessage;
+
+            //тестируем
+            myMessanger.SendMessage("Dima", "Sasha", "Hi!");
+            myMessanger.SendMessage("Sahsa", "Dima", "Hello! How are you?");
+            myMessanger.SendMessage("Vasya", "All", "Hi All!");
+            Console.ReadKey();
+        }
+
+        private static void LogMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine($"{e.SenderName} says \"{e.Message}\" to {e.ReceiverName}");
+        }
+    }
+}
